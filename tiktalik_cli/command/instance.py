be5298d13228b728e3be9ad03b7624d065136ab2
Stanisław Uss <uss.stanislaw@protonmail.com>
2020-01-07 16:29:49 +0100

    instance.py

--- tiktalik_cli/command/instance.py
+++ tiktalik_cli/command/instance.py
@@ -1,20 +1,21 @@
+"""Module tiktalik_cli.command.instance"""
 # Copyright (c) 2013 Techstorage sp. z o.o.
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of 
-# this software and associated documentation files (the "Software"), to deal in 
-# the Software without restriction, including without limitation the rights to 
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
-# the Software, and to permit persons to whom the Software is furnished to do so, 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
 # subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all 
+#
+# The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
-# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
-# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from tiktalik.computing.objects import Instance
@@ -32,12 +33,11 @@ class ListInstances(ComputingCommand):
         p.add_argument("-i", action="store_true", dest="vpsimage", help="Fetch VPS Image details for each instance")
         p.add_argument("-v", dest="verbose", action="store_true", help="Print extra information (flags -a, -i, -t yield more details)")
 
-    return "list"
+        return "list"
 
     def execute(self):
-        instances = Instance.list_all(self.conn, actions=self.args.actions,
-            cost=self.args.cost, vpsimage=self.args.vpsimage)
-        for instance in sorted(instances, key = lambda x: x.hostname):
+        instances = Instance.list_all(self.conn, actions=self.args.actions, cost=self.args.cost, vpsimage=self.args.vpsimage)
+        for instance in sorted(instances, key=lambda x: x.hostname):
             if not self.args.verbose:
                 print(("%s (%s) - %s" % (instance.hostname, instance.uuid, "Running" if instance.running else "Not running")))
             else:
@@ -52,11 +52,9 @@ class CreateInstance(ComputingCommand):
         p.add_argument("image_uuid", help="UUID of a VPS Image that should be used.")
         p.add_argument("size", help='Instance size (in units). Allowed values: 0.25, 0.5, 1 - 15, "cpuhog", "cpuhog4", "1s", "2s", "4s", "8s" and "16s".')
         p.add_argument("hostname", help="Hostname set at installation time.")
-        p.add_argument("-n", dest="networks", metavar="NETWORK_NAME", action="append",
-            help="Attach these networks to the new instance. Use the list-networks command to list available networks.")
+        p.add_argument("-n", dest="networks", metavar="NETWORK_NAME", action="append", help="Attach these networks to the new instance. Use the list-networks command to list available networks.")
         p.add_argument("-b", dest="batch_mode", action="store_true", help="Batch mode. Don't confirm the operation.")
-        p.add_argument("-d", dest="disk_size_gb", action="store", type=int,
-            help="For standard instances must set disk size in GB.")
+        p.add_argument("-d", dest="disk_size_gb", action="store", type=int, help="For standard instances must set disk size in GB.")
 
         return "create-instance"
 
@@ -97,8 +95,7 @@ class CreateInstance(ComputingCommand):
             self.yesno(
                 "Creating new instance with these parameters:\n"
                 "Image UUID: %s\nSize: %s\nHostname: %s\nNetworks: %s\n"
-                "Is this OK?" % (self.args.image_uuid, size, self.args.hostname,
-                    ", ".join(self.args.networks)))
+                "Is this OK?" % (self.args.image_uuid, size, self.args.hostname, ", ".join(self.args.networks)))
 
         response = self.conn.create_instance(self.args.hostname, size, \
                         self.args.image_uuid, networks, disk_size_gb=disk_size_gb)
@@ -141,12 +138,9 @@ class InstanceCommand(ComputingCommand):
     @classmethod
     def add_common_arguments(cls, parser):
         group = parser.add_mutually_exclusive_group()
-        group.add_argument("instance", nargs='?', default='',
-                help="Instance hostname prefix, or instance UUID prefix.")
-        group.add_argument("-n", dest="hostname",
-                help="Instance full hostname. Use -u or `instance' argument if the hostname is not unique.")
-        group.add_argument("-u", dest="uuid",
-                help="Instance full UUID. Can be specified instead of name (-n) or `instance' argument.")
+        group.add_argument("instance", nargs='?', default='', help="Instance hostname prefix, or instance UUID prefix.")
+        group.add_argument("-n", dest="hostname", help="Instance full hostname. Use -u or `instance' argument if the hostname is not unique.")
+        group.add_argument("-u", dest="uuid", help="Instance full UUID. Can be specified instead of name (-n) or `instance' argument.")
 
     def _instance_from_args(self, actions=False, vpsimage=False, cost=False):
         if self.args.hostname and self.args.uuid:
@@ -171,22 +165,19 @@ class InstanceCommand(ComputingCommand):
                         print((" - %s (%s)" % (i.hostname, i.uuid)))
                     raise CommandError("Select exactly one instance.")
 
-                instance = Instance.get_by_uuid(self.conn, instances[0].uuid,
-                        actions, vpsimage, cost)
+                instance = Instance.get_by_uuid(self.conn, instances[0].uuid, actions, vpsimage, cost)
             # select by full hostname only:
             elif self.args.hostname:
-                instances = Instance.get_by_hostname(self.conn, self.args.hostname,
-                        actions, vpsimage, cost)
+                instances = Instance.get_by_hostname(self.conn, self.args.hostname, actions, vpsimage, cost)
                 if len(instances) > 1:
                     msg = ", ".join(i.uuid for i in instances)
-                    raise CommandError("There are %s instances matching hostname %s: %s"
-                            % (len(instances), self.args.hostname, msg))
+                    raise CommandError("There are %s instances matching hostname %s: %s" % (len(instances), self.args.hostname, msg))
 
                 instance = instances[0]
             # select by full uuid only:
             else:
                 instance = Instance.get_by_uuid(self.conn, self.args.uuid,
-                        actions, vpsimage, cost)
+                                                actions, vpsimage, cost)
 
             return instance
         except TiktalikAPIError as ex:
@@ -198,8 +189,8 @@ class InstanceCommand(ComputingCommand):
 class StartInstance(InstanceCommand):
     @classmethod
     def add_parser(cls, parent, subparser):
-        p = subparser.add_parser("start", description="Start an instance. Either name or UUID must be specified.", 
-                parents=[parent])
+        p = subparser.add_parser("start", description="Start an instance. Either name or UUID must be specified.",
+                                 parents=[parent])
         InstanceCommand.add_common_arguments(p)
         return "start"
 
@@ -213,11 +204,11 @@ class StopInstance(InstanceCommand):
     @classmethod
     def add_parser(cls, parent, subparser):
         p = subparser.add_parser("stop",
-                description="Stop an instance. Send ACPI Shutdown signal (default), or stop forcefully (see -f argument)",
-                parents=[parent])
+                                 description="Stop an instance. Send ACPI Shutdown signal (default), or stop forcefully (see -f argument)",
+                                 parents=[parent])
         InstanceCommand.add_common_arguments(p)
         p.add_argument("-f", dest="force", action="store_true",
-                help="Stop the instance forcefully. Do not send gently ACPI Shutdown signal, instead cut the power off.")
+                       help="Stop the instance forcefully. Do not send gently ACPI Shutdown signal, instead cut the power off.")
 
         return "stop"
 
@@ -236,19 +227,18 @@ class StopInstance(InstanceCommand):
 class BackupInstance(InstanceCommand):
     @classmethod
     def add_parser(cls, parent, subparser):
-        p = subparser.add_parser("backup", description="Backup an instance. Either name or UUID must be specified.", 
-                parents=[parent])
+        p = subparser.add_parser("backup", description="Backup an instance. Either name or UUID must be specified.", parents=[parent])
         InstanceCommand.add_common_arguments(p)
-        p.add_argument("--set_name", type=str,  help="Set name of your backup", )
+        p.add_argument("--set_name", type=str, help="Set name of your backup", )
         return "backup"
 
     def execute(self):
         instance = self._instance_from_args()
         if instance.running:
             raise CommandError("Instance is running. Please stop it before starting backup.")
-                if self.args.set_name:
+        if self.args.set_name:
             instance.backup(self.args.set_name)
-                else:
+        else:
             instance.backup()
         print(("Instance %s (%s) is now being backed up" % (instance.hostname, instance.uuid)))
 
@@ -257,14 +247,14 @@ class DeleteInstance(InstanceCommand):
     @classmethod
     def add_parser(cls, parent, subparser):
         p = subparser.add_parser("delete-instance",
-                description="Permanently remove an instance. Either name or UUID must be specified.", 
-                parents=[parent])
+                                 description="Permanently remove an instance. Either name or UUID must be specified.",
+                                 parents=[parent])
         InstanceCommand.add_common_arguments(p)
         return "delete-instance"
 
     def execute(self):
         instance = self._instance_from_args()
-        
+
         self.conn.delete_instance(instance.uuid)
         print(("Instance %s (%s) is now being removed" % (instance.hostname, instance.uuid)))
 
@@ -274,7 +264,7 @@ class InstanceInfo(InstanceCommand):
     def add_parser(cls, parent, subparser):
         p = subparser.add_parser("info", description="Display instance information.", parents=[parent])
         p.add_argument("-a", "--all", dest="verbose", action="store_true",
-            help="Print all extra information.")
+                       help="Print all extra information.")
         InstanceCommand.add_common_arguments(p)
         return "info"
 
@@ -306,8 +296,8 @@ class RemoveInterface(InstanceCommand):
     @classmethod
     def add_parser(cls, parent, subparser):
         p = subparser.add_parser("remove-interface",
-                description="Removes network interface from an instance.",
-                parents=[parent])
+                                 description="Removes network interface from an instance.",
+                                 parents=[parent])
         p.add_argument("seq_or_ip", help="Interface name (eg \"eth1\") or IP address assign to interface to be removed.")
         InstanceCommand.add_common_arguments(p)
 
@@ -315,7 +305,7 @@ class RemoveInterface(InstanceCommand):
 
     def execute(self):
         instance = self._instance_from_args()
-        
+
         seq_str = self.args.seq_or_ip.replace('eth', '')
         iface_uuid = None
         for iface in instance.interfaces:
@@ -326,5 +316,3 @@ class RemoveInterface(InstanceCommand):
             raise CommandError("Cannot find interface by seq nor by IP address.")
 
         instance.remove_interface(iface_uuid)
-
-
